3e849d906ea5a9515b9bf1534f1ef240583b864c
cpu372 <cpu372@cpu372s-mac-mini.local>
2016-10-10 11:31:49 +0530

    Initial Commit

---
+++ SampleAppGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleAppGit
+//
+//  Created by cpu372 on 10/10/16.
+//  Copyright © 2016 Example. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleAppGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleAppGit
+//
+//  Created by cpu372 on 10/10/16.
+//  Copyright © 2016 Example. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
